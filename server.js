125f849d6092290aac57261018196410ee805b59
m0hammadzaib <zaibfr4@gmail.com>
2026-03-08 05:41:08 +0530

    Defined Function

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
-const app = express()
-dotenv.config()
-const port = 3000
+import pool from './src/config/db.js'
 
-app.get('/', (req, res) => {
-  res.send('Hello World Zaib Bro....!')
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("AuthCore API Running")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+pool.connect()
+  .then(() => {
+    console.log("PostgreSQL connected")
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err)
+  })
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
