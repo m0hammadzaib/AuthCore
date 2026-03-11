95db76f8d1cd6c493f249e9600df0f40bcfb0040
m0hammadzaib <zaibfr4@gmail.com>
2026-03-11 14:50:35 +0530

    Authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,11 @@
 import pool from "../config/db.js"
+
+export const registerUser = async (req,res)=>{
+    try{
+        const {email,username,password} = req.body
+
+        const result = await pool.query(
+            "INSERT INTO users (email, username, password) VALUES ($1,$2,$3) RETURNING * ",[email,username,password]
+        )
+    }
+}
