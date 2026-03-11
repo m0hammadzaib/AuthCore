84478b22a9a9cd4b87a56af8dcd71ad46cb16a4f
m0hammadzaib <zaibfr4@gmail.com>
2026-03-11 15:24:56 +0530

    Authentication file

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,5 +7,13 @@ export const registerUser = async (req,res)=>{
         const result = await pool.query(
             "INSERT INTO users (email, username, password) VALUES ($1,$2,$3) RETURNING * ",[email,username,password]
         )
+
+        res.status(201).json({
+            message:"User registered",
+            user : result.rows[0]
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error:"Server error"})
     }
 }
