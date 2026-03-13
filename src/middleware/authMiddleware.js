a4c19a8fe883d01672da6d859b7fc6e68f653831
m0hammadzaib <zaibfr4@gmail.com>
2026-03-13 16:07:27 +0530

    web token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,18 @@
 import jwt from 'jsonwebtoken'
 
 export function authMiddleware(res,req,next){
-    
+
     const token = req.headers.authrorization
+
+    if(!token){
+        return res.status(401).json({message:"No token"})
+    }
+    try{
+        const decoded = jwt.verify(token, provess.env.JWT_SECRET)
+
+        req.user = decoded
+        next()
+    }catch(err){
+        res.status
+    }
 }
