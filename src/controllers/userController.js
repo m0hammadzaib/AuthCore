9b966c341a4479cb44e562758497f246513d433e
m0hammadzaib <zaibfr4@gmail.com>
2026-03-20 22:08:41 +0530

    updated userRoutes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,26 @@
 import pool from "../config/db";
 
 const getCurrentUser= async (req,res)=>{
-    
+    try{
+        const userID = req.user.id;
+
+        const result = await pool.query(
+            'SELECT id,email,username,role,created_at FROM users WHERE id=$1 AND is_deleted = false ',[userID]
+        );
+        if(result.rows.length===0){
+            return res.status(404).json({
+                error:"User not found"
+            })
+        }
+        res.status(200).json({
+            user:result.rows[0]
+        })
+    }catch(err){
+           console.log(err)
+           res.status(500).json({
+             error:"Server error"
+           })
+    }
 }
+
+export default getCurrentUser
--- src/route/userRoutes.js
+++ src/route/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/authMiddleware.js"
+import getCurrentUser from "../controllers/userController.js"
 
 const router = express.Router()
 
@@ -7,7 +8,7 @@ router.get("/me", authMiddleware,(req,res)=>{
    res.json({
     message:"Protected route accessed",
     user:req.user
-   })
+   }), getCurrentUser
 })
 
 export default router
