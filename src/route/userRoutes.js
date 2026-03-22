f42f18b4ad8d663cec509d4a9afedde140e6631f
m0hammadzaib <zaibfr4@gmail.com>
2026-03-22 17:31:45 +0530

    Another Commit

--- src/route/userRoutes.js
+++ src/route/userRoutes.js
@@ -1,6 +1,8 @@
 import express from "express"
 import authMiddleware from "../middleware/authMiddleware.js"
 import getCurrentUser from "../controllers/userController.js"
+import isAdmin from "../middleware/isAdmin.js"
+import pool from "../config/db.js"
 
 const router = express.Router()
 
@@ -9,6 +11,18 @@ router.get("/me", authMiddleware,(req,res)=>{
     message:"Protected route accessed",
     user:req.user
    }), getCurrentUser
+})
+
+router.get("/all",authMiddleware,isAdmin, async (res,req)=>{
+   try{
+      const result = await pool.query("SELECT id,email,username,role FROM user WHERE is_deleted = false");
+
+      res.json({
+         user:result.rows
+      })
+   }catch(error){
+         res.status(500).json({error:"Server Error"})
+   }
 })
 
 export default router
