30735f95cbdf13e5353c30c29648fa3bf40f5647
m0hammadzaib <zaibfr4@gmail.com>
2026-03-23 16:43:39 +0530

    complete core app features

--- src/route/userRoutes.js
+++ src/route/userRoutes.js
@@ -1,28 +1,29 @@
-import express from "express"
-import authMiddleware from "../middleware/authMiddleware.js"
-import getCurrentUser from "../controllers/userController.js"
-import isAdmin from "../middleware/isAdmin.js"
-import pool from "../config/db.js"
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import getCurrentUser from "../controllers/userController.js";
+import isAdmin from "../middleware/isAdmin.js";
+import pool from "../config/db.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/me", authMiddleware,(req,res)=>{
-   res.json({
-    message:"Protected route accessed",
-    user:req.user
-   }), getCurrentUser
-})
+// ✅ Get current user
+router.get("/me", authMiddleware, getCurrentUser);
 
-router.get("/all",authMiddleware,isAdmin, async (res,req)=>{
-   try{
-      const result = await pool.query("SELECT id,email,username,role FROM user WHERE is_deleted = false");
+// ✅ Admin: get all users
+router.get("/all", authMiddleware, isAdmin, async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT id, email, username, role FROM users WHERE is_deleted = false"
+        );
 
-      res.json({
-         user:result.rows
-      })
-   }catch(error){
-         res.status(500).json({error:"Server Error"})
-   }
-})
+        res.json({
+            users: result.rows
+        });
 
-export default router
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+export default router;
