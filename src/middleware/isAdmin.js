999fd8930a63532e9fbfcf40f8d31c4bafcf0ee6
m0hammadzaib <zaibfr4@gmail.com>
2026-03-23 16:32:10 +0530

    Updated isAdmin.js

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,11 +1,19 @@
-const isAdmin=(res,req,next)=>{
-      if(req.user.role !== "admin"){
-        return res.status(403).json({
-            error:"Access denied. Admin only"
-        })
-      }
 
-      next()
-}
+const isAdmin = (req, res, next) => {
+    if (!req.user) {
+        return res.status(401).json({
+            error: "User not authenticated"
+        });
+    }
+
+    if (req.user.role !== "admin") {
+        return res.status(403).json({
+            error: "Access denied. Admin only."
+        });
+    }
+
+    next();
+};
+
 
 export default isAdmin
